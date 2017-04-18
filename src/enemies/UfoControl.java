8c1dd794a93591630bad17c90502d427d569d500
123\Huy <myheaven291@gmail.com>
2017-04-18 09:45:46 +0700

    no message

--- src/enemies/UfoControl.java
+++ src/enemies/UfoControl.java
@@ -11,31 +11,19 @@ import java.awt.*;
 public class UfoControl {
     private ImageRenderer imageRenderer;
     private GameRect gameRect;
+    private MoveBehavior moveBehavior;
 
-   public UfoControl(int x, int y, Image image){
-       imageRenderer = new ImageRenderer(image);
-       gameRect = new GameRect(x,y,150,100);
-   }
+    public UfoControl(int x, int y, Image image) {
+        imageRenderer = new ImageRenderer(image);
+        gameRect = new GameRect(x, y, 150, 100);
+        moveBehavior = new UfomoveBehavior();
+    }
 
-   public void draw(Graphics graphics){
-       imageRenderer.render(graphics, gameRect);
-   }
-   int cd = 250;
-   public void update(){
-       cd --;
-       if(cd <= 0){
-           if(gameRect.getX() <= 100){
-               gameRect.move(1,-1);
-           } else if(gameRect.getX() <= 300){
-               gameRect.move(1,1);
-           } else if(gameRect.getX() <= 500){
-               gameRect.move(1,-1);
-           } else gameRect.move(1,1);
-       }
-       if(gameRect.getX() >= 800){
-           gameRect.setX(-150);
-           gameRect.setY(150);
-       }
-       System.out.println(gameRect.getX());
-   }
+    public void draw(Graphics graphics) {
+        imageRenderer.render(graphics, gameRect);
+    }
+
+    public void update() {
+        moveBehavior.move(gameRect);
+    }
 }
